3144408459efb942cbad85975b1bc606ca864ae5
Ivan Bielozorov <vanya2143@gmail.com>
2021-02-19 13:14:02 +0200

    The task 'Majority' is completed.

---
+++ Majority.py
@@ -0,0 +1,29 @@
+"""
+https://py.checkio.org/ru/mission/majority/
+
+We have a List of booleans. Let's check if the majority of elements are true.
+Some cases worth mentioning:
+1) an empty list should return false;
+2) if trues and falses have an equal amount, function should return false.
+"""
+
+
+def is_majority(items: list) -> bool:
+    t = items.count(True)
+    f = items.count(False)
+    return True if t > f else False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_majority([True, True, False, True, False]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_majority([True, True, False, True, False]) == True
+    assert is_majority([True, True, False]) == True
+    assert is_majority([True, True, False, False]) == False
+    assert is_majority([True, True, False, False, False]) == False
+    assert is_majority([False]) == False
+    assert is_majority([True]) == True
+    assert is_majority([]) == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
